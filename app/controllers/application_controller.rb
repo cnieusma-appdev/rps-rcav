15631ac4bc2c8ab3c83c59ae8539591e00b14ede
cnieusma <cnieusma@uchicago.edu>
2023-01-25 02:02:10 +0000

    fix rock page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,16 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
     render({ :template => "game_templates/user_rock.html.erb"})
   end
 
